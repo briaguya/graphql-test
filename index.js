814644a2a304f61e767bc139291cd6b4a98ac97f
Brian Smith <briaguya@gmail.com>
2017-09-15 04:02:27 -0500

    Add some todos for people to implement

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ import _ from 'lodash';
 var schema = buildSchema(`
   type Query {
     numberRange(start: Int, end: Int!): [Int]
+    evenNumbers(start: Int, end: Int!): [Int]
+    primeNumbers(start: Int, end: Int!): [Int]
+    numberRangeAsWords(start: Int, end: Int!, language: String): [String]
+    somethingFun: String
   }
 `);
 
 var root = {
-  numberRange: ({ start = 0, end }) => _.range(start, end)
-
+  numberRange: ({ start = 0, end }) => _.range(start, end),
+  evenNumbers: ({ start = 0, end }) => _.range(start, end, 2),
+  primeNumbers: ({ start = 0, end }) => [1,3,5,7], // TODO: Implement this
+  numberRangeAsWords: ({ start = 0, end, language = 'en' }) => {
+    // TODO: Implement this
+    if(language === 'en') {
+      return ['one', 'two', 'three']
+    }
+  },
+  somethingFun: () => 'Have a blast!' // TODO: Implement this
 };
 
 var app = express();
