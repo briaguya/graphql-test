3b956185cd27661dd4b580f7ffdc5ecee8169530
Brian Smith <briaguya@gmail.com>
2017-09-15 03:20:40 -0500

    Implement number range

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { buildSchema } from 'graphql';
+import _ from 'lodash';
 
 var schema = buildSchema(`
   type Query {
-    hello: String
+    numberRange(start: Int, end: Int!): [Int]
   }
 `);
 
-var root = { hello: () => 'Hello world!' };
+var root = {
+  numberRange: ({ start = 0, end }) => _.range(start, end)
+
+};
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
